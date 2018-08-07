cdd2a5bfc2409b528e4cd356ff350a83e19659cc
Chaitanya Gadde <chaitanya.gadde232@gmail.com>
2018-08-07 18:53:42 -0400

    Add project files.

---
+++ Merge2PDFs/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Merge2PDFs.Startup))]
+namespace Merge2PDFs
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
